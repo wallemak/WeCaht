4e678dde59a0bc7f042c6836534c3a498eb993ab
nee <nee@taolin.com>
2019-05-09 16:51:25 +0800

    route

---
+++ routes/route.php
@@ -0,0 +1,5 @@
+<?php
+// header("Access-Control-Allow-Origin:*");
+
+$request = $_REQUEST;
+echo json_encode($request);
--- static/js/form.js
+++ static/js/form.js
@@ -2,5 +2,9 @@
 {
 	var reg = new RegExp("(^|&)openid=([^&]*)(&|$)");
 	var search = window.location.search.substr(1).match(reg);
-	var openid = unescape(search[2]);
+	var openid = search==null?null : unescape(search[2]);
+
+	$.get("http://47.106.227.171/routes/route.php",{id:123},function(data){
+		console.log(data);
+	},'jsonp');
 })();
