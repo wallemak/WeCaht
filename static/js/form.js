8d5298e66e056923ceadc4e27c5520ad8af7979c
nee <nee@taolin.com>
2019-05-13 17:10:09 +0800

    关闭当前页

--- static/js/form.js
+++ static/js/form.js
@@ -21,7 +21,29 @@
 			var content = $('#content').val();
 			$.post("http://47.106.227.171/views/form.ajax.php",{openid:openid,content:content,type:'submit'},function(data,status){
 				if(data.errmsg == 'ok'){
-
+						layer.open({
+						title:'',
+						content: '推送成功',
+						btnAlign:'c',
+						shade :0.5,
+						anim: 5,
+						closeBtn :0,
+						yes:function(index,layero){
+							layer.close(index);
+							window.opener=null;
+							window.open('','_self');
+							window.close();
+						}
+					    });
+					}else{
+						layer.open({
+						title:data.error,
+						content: '天寿啦,出错啦~!',
+						btnAlign:'c',
+						shade :0.5,
+						anim: 5,
+						closeBtn :0
+					    });
 				}
 			},'json');
 		}
