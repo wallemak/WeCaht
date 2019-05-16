20488a624e7aad42645b9a4cb8415a24510e7a77
nee <nee@taolin.com>
2019-05-16 16:47:24 +0800

    index

--- index.php
+++ index.php
@@ -23,11 +23,11 @@ if(isset($_GET['echostr'])){
 class WeChat
 {
 
-	public function __construct()
-	{
-		$check = new check;
-		$this->access_token = $check->access_token;
-	}
+	// public function __construct()
+	// {
+	// 	$check = new check;
+	// 	$this->access_token = $check->access_token;
+	// }
 
 	public function valid()
 	{
@@ -98,9 +98,9 @@ class WeChat
 			if($MsgType == 'voice')
 			{
 				$MediaId = $postObj->MediaId;
-				$url = "http://api.weixin.qq.com/cgi-bin/media/voice/queryrecoresultfortext?access_token=$this->access_token&voice_id=MediaId&lang=zh_CN"
-				$result = curl()->result($url);
-				echo $result;
+				// $url = "http://api.weixin.qq.com/cgi-bin/media/voice/queryrecoresultfortext?access_token=$this->access_token&voice_id=MediaId&lang=zh_CN"
+				// $result = curl()->result($url);
+				// echo $result;
 				// $res = json_decode($result,true)['result'];
 				// echo $this->transmitText($postObj,$res);
 			}
