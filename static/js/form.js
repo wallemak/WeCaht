61d536b8fd381ea6be2c2bb219d4b3bc4ed4d19a
nee <nee@taolin.com>
2019-06-03 16:38:32 +0800

    分享到朋友圈

--- static/js/form.js
+++ static/js/form.js
@@ -17,9 +17,8 @@
 		dataType:"jsonp",
 		success:function(data)
 		{
-			console.log(data);
 			wx.config({
-			    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+			    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
 			    appId: 'wx2ca1b4d674248dbd', // 必填，公众号的唯一标识
 			    timestamp: data.time , // 必填，生成签名的时间戳
 			    nonceStr: data.noncestr, // 必填，生成签名的随机串
@@ -29,8 +28,16 @@
 			    	'updateAppMessageShareData'//自定义“分享给朋友”及“分享到QQ”按钮的分享内容
 			    ] // 必填，需要使用的JS接口列表
 			});
-
-
+			wx.ready(function(){
+			    wx.updateTimelineShareData({ 
+		            title: '模板推送测试', // 分享标题
+		            link: 'http://47.106.227.171/views/form.html', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+		            imgUrl: '../static/images/2.jpg', // 分享图标
+		            success: function () {
+		              // 设置成功
+		            }
+		        })
+			});
 		}
 	});
 
