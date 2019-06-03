cecf77ea7c4cd2c9f535f9940b681ddb29bec7a8
nee <nee@taolin.com>
2019-06-03 14:39:30 +0800

    update .gitignore

--- static/js/form.js
+++ static/js/form.js
@@ -17,11 +17,12 @@
 		dataType:"jsonp",
 		success:function(data)
 		{
+			console.log(data);
 			wx.config({
 			    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
 			    appId: 'wx2ca1b4d674248dbd', // 必填，公众号的唯一标识
 			    timestamp: data.time , // 必填，生成签名的时间戳
-			    nonceStr: data.nee5cf490bf83e2a, // 必填，生成签名的随机串
+			    nonceStr: data.noncestr, // 必填，生成签名的随机串
 			    signature: data.signature,// 必填，签名
 			    jsApiList: [
 			    	'updateTimelineShareData',//自定义“分享到朋友圈”及“分享到QQ空间”按钮的分享内容
@@ -29,10 +30,10 @@
 			    ] // 必填，需要使用的JS接口列表
 			});
 
+
 		}
 	});
 
- 
 	// $.get("http://192.168.131.1/mak/weixin/views/form.ajax.php",{openid:openid,type:'GetUserinfo'},function(data,status){
 	// 	new Vue({ el:'#headimgurl',data:{data} });
 	// 	$('#hid').attr('value',data.openid);
